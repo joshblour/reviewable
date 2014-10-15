6b76eb4382ac2044577e8cb94cd47de8fbbb221b
Yonah Forst <joshblour@hotmail.com>
2014-10-15 10:21:35 +0200

    find review by reviewer

--- lib/reviewable/acts_as_reviewable.rb
+++ lib/reviewable/acts_as_reviewable.rb
@@ -9,15 +9,19 @@ module Reviewable
     module ClassMethods
       def acts_as_reviewable
         include Reviewable::ActsAsReviewable::LocalInstanceMethods
-        
+  
         has_many :reviews, as: :reviewable_object, class_name: 'Reviewable::Review'
-        # has_many :reviewers, through: :reviews
-                
+
         accepts_nested_attributes_for :reviews, allow_destroy: true
       end
     end
     
     module LocalInstanceMethods
+      
+      def review_from reviewer
+        reviews.where(reviewer: reviewer).first
+      end
+      
     end
   end
 end
--- spec/models/acts_as_reviewable_spec.rb
+++ spec/models/acts_as_reviewable_spec.rb
@@ -6,6 +6,12 @@ module Reviewable
     it { should have_many(:reviews) }
     it { should accept_nested_attributes_for(:reviews) }
     
-    
+    it 'find review by reviewer' do
+      review = create(:review)
+      product = review.reviewable_object
+      user = review.reviewer
+      
+      expect(product.review_from(user)).to eq review
+    end
   end
 end
